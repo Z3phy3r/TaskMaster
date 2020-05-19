da81ffba7cc7b4f8165c33f63ad9544adbfb955a
Guido <Guidolrotteveel@gmail.com>
2020-05-19 09:58:27 +0200

    sdk sincroniced

--- dev/app/src/main/java/nl/builders/taskmaster/MainActivity.kt
+++ dev/app/src/main/java/nl/builders/taskmaster/MainActivity.kt
@@ -1,14 +1,20 @@
 package nl.builders.taskmaster
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
+private var mStorageRef: StorageReference? = null
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mStorageRef = FirebaseStorage.getInstance().reference;
         Log.e("hallo", "hallo")
 
 Log.i("doei", "doei")
