79284076d21f3f1d63055a7e4117154a3d204beb
Daniel <daboven@gmail.com>
2020-06-08 12:20:11 +0200

    Taskscreen added

--- dev/app/src/main/java/nl/builders/taskmaster/MainActivity.kt
+++ dev/app/src/main/java/nl/builders/taskmaster/MainActivity.kt
@@ -7,13 +7,7 @@ import com.example.taksmasterapp.submissions
 import com.google.firebase.database.*
 import com.google.firebase.storage.StorageReference
 
-var setTasks= arrayListOf<TaskQuestion>()
 
-data class TaskQuestion(
-        val discription: String?="",
-        val imagereff: String?="",
-        val name: String?=""
-)
 
 class MainActivity : AppCompatActivity() {
     private var database: DatabaseReference? = null
@@ -23,27 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val db = FirebaseDatabase.getInstance()
         setContentView(R.layout.activity_main)
-        getTasks()
     }
-    private fun getTasks() {
-    FirebaseDatabase.getInstance().getReference("tasks")
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {
-                    throw p0.toException()
-                }
 
-                override fun onDataChange(p0: DataSnapshot) {
-                    for (productSnapshot in p0.children) {
-                        val tasks =productSnapshot.getValue((TaskQuestion()::class.java))
-                        setTasks.add(tasks!!)
-                        Log.e("setTasks", setTasks.toString())
-                        Log.e("p0",productSnapshot.toString())
-                    }
-                }
-            })
-
-    }
 
 }
